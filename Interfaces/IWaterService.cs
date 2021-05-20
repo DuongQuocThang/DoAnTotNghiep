7664c8b34b8b008ba6d0c512c6460ed79b8f9a24
ThanhDuongNguyen <nguyenthanhduonglklklk@gmail.com>
2021-05-20 07:10:38 +0700

    Add project files.

---
+++ Interfaces/IWaterService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebDoAn.Models;
+
+namespace WebDoAn.Interfaces
+{
+    public interface IWaterService
+    {
+        public event WaterDelegate OnWaterChanged;
+        IList<WaterData> GetCurrentWater();
+    }
+}
