65a5b3d05cd0f4e3de3a8270870570ecbed6c2bb
DESKTOP-GV370AV\duong <duongquocthang2629@gmail.com>
2021-06-11 10:34:01 +0700

    update 11/6/2021

---
+++ Data/TelemetryService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDoAn.Data
+{
+    public class TelemetryService
+    {
+        public event EventHandler<TelemetryDataPoint> InputMessageReceived;
+
+        private async Task OnInputMessageReceived(TelemetryDataPoint telemetryDataPoint)
+        {
+            await Task.Run(() => { InputMessageReceived?.Invoke(this, telemetryDataPoint); });
+        }
+
+        public async Task SendTelemetry(TelemetryDataPoint telemetryDataPoint)
+        {
+            if (telemetryDataPoint != null)
+            {
+                await OnInputMessageReceived(telemetryDataPoint);
+            }
+        }
+    }
+
+    public class TelemetryDataPoint
+    {
+        public string Temperature { get; set; }
+        public string Turbidity { get; set; }
+        public string PH { get; set; }
+        public string Waterflow { get; set; }
+    };
+}
